a79045c7b55ba62a3091864fd48a25e53b422551
LautaroLeall <lautaroleal4@gmail.com>
2026-01-27 19:26:45 -0300

    delete files before

--- src/context/authContext.js
+++
@@ -1,7 +0,0 @@
-// src/context/authContext.js
-// - Solo crea y exporta el contexto (sin componentes).
-// - Separarlo evita advertencias de Fast Refresh al mezclar componentes y otras exportaciones.
-
-import { createContext } from "react";
-
-export const AuthContext = createContext(null);
