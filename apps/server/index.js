dc9ed2d6dc959af358efb0172436049236ef65be
LautaroLeall <lautaroleal4@gmail.com>
2026-02-03 20:09:05 -0300

    implementando back (supabase) prisma

--- apps/server/index.js
+++ apps/server/index.js
@@ -21,5 +21,5 @@ app.get("/api/health", (_, res) => {
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () =>
-    console.log(`Servidor corriendo en puerto ${PORT}`)
+    console.log(`Servidor corriendo en puerto http://localhost:${PORT} 🚀`)
 );
