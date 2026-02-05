4660bdafc982c177dadbc409949d3e3d47125bb9
LautaroLeall <lautaroleal4@gmail.com>
2026-02-05 19:07:47 -0300

    call router in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
+import Router from "./routes/Router"
+
 export default function App() {
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-black">
-            <h1 className="text-4xl font-bold text-green-400">
-                Tailwind OK 🚀
-            </h1>
-        </div>
-    )
+    return <Router />
 }
